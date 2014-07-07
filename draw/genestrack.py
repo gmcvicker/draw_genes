e78df8e56711c4203a612da139746dcaf78dba3b
Graham McVicker <gmcvicker@gmail.com>
2014-07-06 18:22:56 -0700

    untested modifications to allow only longest isoforms to be drawn

--- draw/genestrack.py
+++ draw/genestrack.py
@@ -2,6 +2,7 @@
 import sys
 
 from genome import coord
+import genome.gene
 from track import Track
 from transcripttrack import TranscriptTrack
 
@@ -20,12 +21,35 @@ class GenesTrack(Track):
         self.row_assignment = None
         self.color = options['color']
         self.utr_color = options['utr_color']
-        
+
+        if 'draw_label' in options:
+            self.draw_label = self.parse_bool_str(options['draw_label'])
+        else:
+            self.draw_label = True
+
         # get all transcripts that overlap region
         self.overlap_trs = coord.get_coord_overlaps(self.region,
                                                     self.transcripts,
                                                     use_strand=False)
 
+        if 'longest_isoform_only' in options:
+            longest_only = self.parse_bool_str(options['longest_isoform_only'])
+        else:
+            longest_only = False
+
+        if longest_only:
+            sys.stderr.write("  only drawing longest isoform of each gene\n")
+
+            gene_groups = gene.group_transcripts(self.overlap_trs)
+
+            sys.stderr.write("  grouped %d transcripts into %d genes\n" % (len(self.overlap_trs), len(gene_groups)))
+            
+            longest_trs = []
+            for gene in gene_groups:
+                longest_trs.append(gene.get_longest_transcript())
+
+            self.overlap_trs = longest_trs
+            
         # assign rows to the transcripts
         padding = region.length() * 0.2
         self.assign_feature_rows(self.overlap_trs, padding=padding)
@@ -44,10 +68,17 @@ class GenesTrack(Track):
 
         for tr in self.overlap_trs:
             # give each transcript its own track
+
+            if self.draw_label:
+                draw_label_str = "true"
+            else:
+                draw_label_str = "false"
+            
             tr_options = {'color' : self.color,
                           'utr_color' : self.utr_color,
                           'border' : 'false',
-                          'height' : str(tr_height)}
+                          'height' : str(tr_height),
+                          'draw_label' = draw_label_str}
             
             tr_track = TranscriptTrack(tr, self.region, tr_options)
 
